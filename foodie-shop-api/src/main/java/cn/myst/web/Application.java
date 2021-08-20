6698c643a50a316c9e9270f3d916e7fb728cf48c
ziming.xing <ziming.xing@proaimltd.com>
2021-08-20 17:44:57 +0800

    开启使用redis作为spring session，去除SpringSecurity权限自动装配

--- foodie-shop-api/src/main/java/cn/myst/web/Application.java
+++ foodie-shop-api/src/main/java/cn/myst/web/Application.java
@@ -2,21 +2,24 @@ package cn.myst.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * @author ziming.xing
  * Create Date：2021/5/12
  */
-@SpringBootApplication
+// 去除SpringSecurity权限自动装配
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描 mybatis 通用 mapper 所在的包
 //@MapperScan(basePackages = "cn.myst.web.**.mapper")
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"cn.myst.web", "org.n3r.idworker"})
 //@EnableTransactionManagement
 @EnableScheduling       // 开启定时任务
+@EnableRedisHttpSession // 开启使用redis作为spring session
 public class Application {
 
     public static void main(String[] args) {
---
+++ foodie-shop-api/src/main/java/cn/myst/web/controller/example/ExampleController.java
@@ -0,0 +1,34 @@
+package cn.myst.web.controller.example;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author ziming.xing
+ * Create Date：2021/8/20
+ */
+//@ApiIgnore
+@Slf4j
+@RestController
+@RequestMapping("example")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class ExampleController {
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+//        session.removeAttribute("userInfo");
+        return "ok";
+    }
+
+}
